dc6bae3b2355c054c96e61551b915a1aac2371e3
Aidan Ferguson <29628351+aidan-ferguson@users.noreply.github.com>
2023-09-23 17:26:20 +0100

    Lighting and shadows

--- main.cpp
+++ main.cpp
@@ -16,14 +16,16 @@
 const int PROGRESS_BAR_WIDTH = 100;
 
 // Rendering options
-const int WIDTH = 800;
-const int HEIGHT = 600;
+const int WIDTH = 420;
+const int HEIGHT = 380;
 const float H_FOV = 90.0f * (M_PI / 180);
 const float V_FOV = 2 * std::atan(std::tan(H_FOV/2.0f)*((float)HEIGHT/(float)WIDTH));
 const int N_SAMPLES = 8;
 const unsigned int MAX_THREADS = 40;
-const unsigned int N_FRAMES = 350;
+const unsigned int N_FRAMES = 350; // 350 @ 20ms per frame -> full rotation
 const unsigned int MS_PER_FRAME = 20; // Number of (in-sim) milliseconds per frame
+const glm::vec3 light_pos(0, -10, 3);
+const float DIFFUSE_LIGHTING = 0.2f;
 
 // Random stuff used for super-sampling
 std::random_device rd;
@@ -70,29 +72,18 @@ float min_dist_to_scene(glm::vec3 pos) {
 }
 
 // Take a normalised ray direction and return the number of steps the ray took into the scene
-int march_ray(glm::vec3 ray_dir, float time){
-    glm::vec3 ray_pos = CAM_POS;
-
-    // Rotate the camera around the Y axis with time
-    glm::mat3 pos_rot_mat = {
-        {std::cos(time) , 0, std::sin(time)},
-        {0              , 1, 0},
-        {-std::sin(time), 0, std::cos(time)}
-    };
-    ray_pos = pos_rot_mat * ray_pos;
-    ray_dir = pos_rot_mat * ray_dir;
-
+std::tuple<int, glm::vec3> march_ray(glm::vec3 ray_pos, glm::vec3 ray_dir, float time){
     int n_steps = 0;
     for(int step = 0; step < MAX_RAY_STEPS; step++) {
         // Step forward maximum allowed amount
         float min_dist = min_dist_to_scene(ray_pos);
         if(min_dist < STEP_EPSILON_THRESHOLD) 
-            return n_steps;
+            return {n_steps, ray_pos};
         ray_pos += ray_dir*min_dist;
         n_steps++;
     }
 
-    return n_steps;
+    return {n_steps, ray_pos};
 }
 
 glm::vec3 get_pixel(int x, int y, float time) {
@@ -108,14 +99,55 @@ glm::vec3 get_pixel(int x, int y, float time) {
         float screen_y = SCREEN_TOP_LEFT.y + (SCREEN_BOTTOM_RIGHT.y - SCREEN_TOP_LEFT.y)*y_percentage; 
         glm::vec3 ray_dir = glm::normalize(glm::vec3(screen_x, screen_y, -1));
 
+        // Rotate the camera around the Y axis with time
+        glm::mat3 pos_rot_mat = {
+            {std::cos(time) , 0, std::sin(time)},
+            {0              , 1, 0},
+            {-std::sin(time), 0, std::cos(time)}
+        };
+
+        // Zoom in with time
+        glm::vec3 ray_pos = CAM_POS;
+        ray_pos.z = ((std::cos(time)+1.0f)/2.0f) + 1.1f;
+
+        // Rotate with time
+        ray_pos = pos_rot_mat * ray_pos;
+        ray_dir = pos_rot_mat * ray_dir;
+
         // Iteratively step the ray forward and return the steps taken
-        pixel += ((float)march_ray(ray_dir, time)/(float)MAX_RAY_STEPS);
+        int march_steps = 0;
+        glm::vec3 march_position;
+        std::tie(march_steps, march_position) = march_ray(ray_pos, ray_dir, time);
+        float current_sample = 1.0f;//(static_cast<float>(march_steps)/static_cast<float>(MAX_RAY_STEPS));
+
+        // Now that we have the position of the intersection if it is not MAX_RAY_STEPS, try to ray-march towards the light source, if it reaches MAX_RAY_STEPS
+        //  multiply the colour of the pixel with the number of steps taken (as a percentage of MAX_RAY_STEPS)
+        if (march_steps < MAX_RAY_STEPS)
+        {
+            glm::vec3 light_dir = glm::normalize(light_pos - march_position);
+            march_position += (light_dir * STEP_EPSILON_THRESHOLD);
+            std::tie(march_steps, march_position) = march_ray(march_position, light_dir, time);
+
+            // Get the number of steps taken towards the light as a percentage of MAX_RAY_STEPS -> then do 1 - that as pixel values are currently flipped (i.e.) 1 = black
+            float light_steps = (static_cast<float>(march_steps)/static_cast<float>(MAX_RAY_STEPS));
+
+            current_sample *= light_steps;
+
+            // Add diffuse lighting
+            current_sample += DIFFUSE_LIGHTING;
+        }
+        else
+        {
+            current_sample = 0.0f;
+        }
+
+        pixel += current_sample;
     }
 
     // Average pixel
     pixel /= N_SAMPLES;
 
-    return (glm::vec3(1,1,1) - pixel);
+    return glm::min(pixel, glm::vec3(1.0f));
 }
 
 void thread_compute_region(glm::vec3** pixels, uint32_t start_y, uint32_t end_y, float time)
@@ -130,10 +162,10 @@ void thread_compute_region(glm::vec3** pixels, uint32_t start_y, uint32_t end_y,
 
 void print_progress(int frame_idx)
 {
-    float progress = static_cast<float>(frame_idx)/static_cast<float>(N_FRAMES);
+    float progress = static_cast<float>(frame_idx+1)/static_cast<float>(N_FRAMES);
 
     std::string progress_string("Frame: (");
-    progress_string += std::to_string(frame_idx) + "/" + std::to_string(N_FRAMES) + ") ";
+    progress_string += std::to_string(frame_idx+1) + "/" + std::to_string(N_FRAMES) + ") ";
     std::string filled_bar(static_cast<int>(PROGRESS_BAR_WIDTH*progress), '#');
     progress_string += "[" + filled_bar + std::string(PROGRESS_BAR_WIDTH-filled_bar.size(), '-') + "]";
 
@@ -156,6 +188,7 @@ int main() {
 
         // Calculate timestamp based on frame 
         float time = static_cast<float>(frame_idx * MS_PER_FRAME) / 1000.0f;
+        // time = static_cast<float>(20 * MS_PER_FRAME) / 1000.0f;
 
         // Start N threads that each populate a portion of the pixels
         std::vector<std::thread> threads;
@@ -193,6 +226,8 @@ int main() {
         }
         file.close();
     }
+
+    printf("\nComplete.\n");
 
     // No explicit delete[] 's as the OS will cleanup
     return 0;
