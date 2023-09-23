b946be3caddd528411ed3f8eae729444ac805f17
Aidan Ferguson <29628351+aidan-ferguson@users.noreply.github.com>
2023-09-23 15:16:00 +0100

    progress bar

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include <glm/glm.hpp>
 #include <vector>
 #include <fstream>
 #include <math.h>
 #include <random>
 #include <thread>
 
+#include <glm/glm.hpp>
+
+// Right-handed coordinate system:
+//  +X -> West
+//  +Y -> Up
+//  +Z -> North
+
+// General options
+const int PROGRESS_BAR_WIDTH = 100;
+
 // Rendering options
-const int WIDTH = 100;
-const int HEIGHT = 100;
+const int WIDTH = 800;
+const int HEIGHT = 600;
 const float H_FOV = 90.0f * (M_PI / 180);
 const float V_FOV = 2 * std::atan(std::tan(H_FOV/2.0f)*((float)HEIGHT/(float)WIDTH));
 const int N_SAMPLES = 8;
-const unsigned int MAX_THREADS = 1;
-const unsigned int N_FRAMES = 30;
+const unsigned int MAX_THREADS = 40;
+const unsigned int N_FRAMES = 350;
 const unsigned int MS_PER_FRAME = 20; // Number of (in-sim) milliseconds per frame
 
 // Random stuff used for super-sampling
@@ -25,11 +34,6 @@ std::uniform_real_distribution<> random_dist(-0.5, 0.5);
 const glm::vec3 CAM_POS = glm::vec3(0, 0, 3);
 const glm::vec3 SCREEN_TOP_LEFT = glm::vec3(-std::tan(0.5f * H_FOV), -std::tan(0.5f * V_FOV), -1);
 const glm::vec3 SCREEN_BOTTOM_RIGHT = glm::vec3(std::tan(0.5f * H_FOV), std::tan(0.5f * V_FOV), -1);
-
-// Right-handed coordinate system:
-//  +X -> West
-//  +Y -> Up
-//  +Z -> North
 
 // Ray marching parameters
 const int MAX_RAY_STEPS = 100;
@@ -76,13 +80,6 @@ int march_ray(glm::vec3 ray_dir, float time){
         {-std::sin(time), 0, std::cos(time)}
     };
     ray_pos = pos_rot_mat * ray_pos;
-
-    // We also need to rotate the direction to point at the center, however the rotation matrix is going to be 180 degrees out of phase
-    // glm::mat3 dir_rot_mat = {
-    //     {std::cos(time - M_PI) , 0, std::sin(time - M_PI)},
-    //     {0              , 1, 0},
-    //     {-std::sin(time - M_PI), 0, std::cos(time - M_PI)}
-    // };
     ray_dir = pos_rot_mat * ray_dir;
 
     int n_steps = 0;
@@ -131,6 +128,19 @@ void thread_compute_region(glm::vec3** pixels, uint32_t start_y, uint32_t end_y,
     }
 }
 
+void print_progress(int frame_idx)
+{
+    float progress = static_cast<float>(frame_idx)/static_cast<float>(N_FRAMES);
+
+    std::string progress_string("Frame: (");
+    progress_string += std::to_string(frame_idx) + "/" + std::to_string(N_FRAMES) + ") ";
+    std::string filled_bar(static_cast<int>(PROGRESS_BAR_WIDTH*progress), '#');
+    progress_string += "[" + filled_bar + std::string(PROGRESS_BAR_WIDTH-filled_bar.size(), '-') + "]";
+
+    printf("%s\r", progress_string.c_str());
+    fflush(stdout);
+}
+
 int main() {
     // Setup
     const uint32_t n_threads = std::min(std::thread::hardware_concurrency(), MAX_THREADS);
@@ -142,8 +152,7 @@ int main() {
 
     for (int frame_idx = 0; frame_idx < N_FRAMES; frame_idx++)
     {
-        printf("Frame %d", frame_idx);
-        fflush(stdout);
+        print_progress(frame_idx);
 
         // Calculate timestamp based on frame 
         float time = static_cast<float>(frame_idx * MS_PER_FRAME) / 1000.0f;
