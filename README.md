6a770f8445e7e319adcc971f4722d5d60b3f68e3
Aidan Ferguson <29628351+aidan-ferguson@users.noreply.github.com>
2024-08-03 20:00:54 +0100

    changed to png format, added sigmoid interpolation, added colour, added camera start->end pos

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # C++ Ray-marcher
 
-Simple C++ ray-marcher that outputs a .ppm files.
+Simple C++ ray-marcher that outputs `.png` files.
  - Has random super-sampling
  - Multi-threading (went from 2:16 -> 0:14 on my machine)
  - Shadows
@@ -10,8 +10,17 @@ Simple C++ ray-marcher that outputs a .ppm files.
 The following image is a 1920x1080 Mandlebulb generated with 8 samples per pixel:
 
 ![samples/1920x1080.png](samples/1920x1080.png)
-
 There are videos in the `samples` sub-directory along with some more images.
+
+Compile with
+```
+g++ main.cpp -Ilib/glm
+```
+
+You can use the following FFMPEG command to create videos from the frames:
+```
+ffmpeg -framerate 30 -i ./frames/%d.png -c:v libx264 -pix_fmt yuv420p output.mp4
+```
 
 ### CUDA
 
@@ -19,5 +28,5 @@ There's also a separate (unoptimised) CUDA implementation. Compared to the CPU m
 
 Compile with:
 ```
-nvcc main.cu -lcurand
+nvcc main.cu -Ilib/stb -lcurand
 ```
--- main.cu
+++ main.cu
@@ -4,6 +4,10 @@
 #include <math.h>
 #include <random>
 #include <thread>
+#include <assert.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 #include <curand_kernel.h>
 #include <cuda_runtime.h>
@@ -17,30 +21,46 @@
 const int PROGRESS_BAR_WIDTH = 100;
 
 // Rendering options
-const int WIDTH = 1920;
-const int HEIGHT = 1080;
+const int WIDTH = 1280;
+const int HEIGHT = 720;
 const float H_FOV = 90.0f * (M_PI / 180);
 const float V_FOV = 2 * std::atan(std::tan(H_FOV/2.0f)*((float)HEIGHT/(float)WIDTH));
 const int N_SAMPLES = 10;
+const unsigned int FRAME_START = 0;
 const unsigned int N_FRAMES = 350; // 350 @ 20ms per frame -> full rotation
 const unsigned int MS_PER_FRAME = 20; // Number of (in-sim) milliseconds per frame
-const float DIFFUSE_LIGHTING = 0.3f;
-const int MAX_CUDA_THREADS = 10'000'000;
+const float T_MAX = ((float)N_FRAMES)*((float)MS_PER_FRAME/1000.0f); // Upper-bound of time
+const int MAX_CUDA_THREADS = 100'000'000;
 
-// Camera, values copied in main
+// Camera and scene values copied in main
 __constant__ float CAM_POS[3];
 __constant__ float SCREEN_TOP_LEFT[3];
 __constant__ float SCREEN_BOTTOM_RIGHT[3];
 __constant__ float LIGHT_POS[3];
+__constant__ float LIGHT_COLOUR[3];
+const float DIFFUSE_LIGHTING = 0.1f;
+const float ZOOM_END = 0.6f; // z-position at the end of the zoom
+const float SLERP_MIN = 0.0f; // start of the sigmoid interpolation range
+const float SLERP_MAX = 6.0f; // end of the sigmoid interpolation range
 
 // Ray marching parameters
 const int MAX_RAY_STEPS = 100;
-const float STEP_EPSILON_THRESHOLD = 0.001f;
+const float STEP_EPSILON_THRESHOLD = 0.00001f;
+
+__device__ float sigmoid_lerp(float p)
+{
+    float lerp = SLERP_MIN * (1-p) + SLERP_MAX * p; // points on the sigmoid curve to interpolate between (controls the speed curve)
+    return 1.0f/(1.0f+exp(-lerp));
+}
 
 // Get the minimum distance to the scene given a point in space
 __device__ float min_dist_to_scene(float pos[3], float time) {
     // Mandlebulb SDF from http://blog.hvidtfeldts.net/index.php/2011/09/distance-estimated-3d-fractals-v-the-mandelbulb-different-de-approximations/
-    float w = sin(time/9.0);
+    // Interpolate through 4th dimension as time goes on, we slow down as time gets close to the end
+    // to do this we sample along a sigmoid curve for the sin input
+    float time_p = time/T_MAX;
+    float w = sin(sigmoid_lerp(time_p));
+
     float z[4] = {pos[0], pos[1], pos[2], w};
     int Iterations = 10;
     float Bailout = 100.0f;
@@ -107,6 +127,29 @@ __device__ void mat_vector_dot(float* mat_a, float* vec, int mat_r, int mat_c, i
 }
 
 
+__device__ void calculate_normal(float pos[3], float normal[3], float time)
+{
+    const float step = 0.001f;
+
+    for (int idx = 0; idx < 3; idx++)
+    {
+        pos[idx] += step;
+        normal[idx] = min_dist_to_scene(pos, time);
+        pos[idx] -= 2*step;
+        normal[idx] -= min_dist_to_scene(pos, time);
+        pos[idx] += step;
+    }
+
+    // Normalise the normal vector
+    float mag = sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);
+    assert(mag != 0);
+    for (int idx = 0; idx < 3; idx++)
+    {
+        normal[idx] /= mag;
+    }
+}
+
+
 __global__ void init_kernel(curandState_t* state)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -114,18 +157,18 @@ __global__ void init_kernel(curandState_t* state)
 }
 
 
-__global__ void compute_pixel(uint8_t* frame, curandState_t* rand_state, float time) {
+__global__ void compute_pixel(unsigned int pixel_offset, uint8_t* frame, curandState_t* rand_state, float time) {
     int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    const int x = thread_idx % WIDTH;
-    const int y = thread_idx / WIDTH; 
+    const int x = (thread_idx+pixel_offset) % WIDTH;
+    const int y = (thread_idx+pixel_offset) / WIDTH; 
 
     // TODO: move outside of kernel
     // Rotate the camera around the Y axis with time
     float pos_rot_mat [3*3] = {
-        std::cos(0.001f) , 0, std::sin(0.001f),
+        std::cos(0.014f) , 0, std::sin(0.014f),
         0              , 1, 0,
-        -std::sin(0.001f), 0, std::cos(0.001f)
+        -std::sin(0.014f), 0, std::cos(0.014f)
     };
 
     // Random super-sampling for each pixel
@@ -149,7 +192,10 @@ __global__ void compute_pixel(uint8_t* frame, curandState_t* rand_state, float t
         // Zoom in with time
         float ray_pos[3];
         memcpy(ray_pos, CAM_POS, sizeof(float)*3);
-        // ray_pos[2] = ((std::cos(time)+1.0f)/2.0f) + 1.1f;
+        // smooth-lerp between two values so we zoom in over time
+        float time_p = time/T_MAX;
+        float lerp = sigmoid_lerp(time_p);
+        ray_pos[2] = ray_pos[2] * (1-lerp) + ZOOM_END * lerp;
 
         // Rotate with time
         mat_vector_dot(pos_rot_mat, ray_pos, 3, 3, 3);
@@ -157,7 +203,6 @@ __global__ void compute_pixel(uint8_t* frame, curandState_t* rand_state, float t
 
         // Iteratively step the ray forward and return the steps taken
         int march_steps = march_ray(ray_pos, ray_dir, time);
-        float current_sample = 1.0f; //(static_cast<float>(march_steps)/static_cast<float>(MAX_RAY_STEPS));
 
         // Now that we have the position of the intersection if it is not MAX_RAY_STEPS, try to ray-march towards the light source, if it reaches MAX_RAY_STEPS
         //  multiply the colour of the pixel with the number of steps taken (as a percentage of MAX_RAY_STEPS)
@@ -170,27 +215,21 @@ __global__ void compute_pixel(uint8_t* frame, curandState_t* rand_state, float t
                 light_dir[idx] /= light_dir_mag;
             }
 
-            ray_pos[0] += (light_dir[0] * STEP_EPSILON_THRESHOLD);
-            ray_pos[1] += (light_dir[1] * STEP_EPSILON_THRESHOLD);
-            ray_pos[2] += (light_dir[2] * STEP_EPSILON_THRESHOLD);
-            march_steps = march_ray(ray_pos, light_dir, time);
+            float normal[3] = {0.0f};
+            calculate_normal(ray_pos, normal, time);
 
-            // Get the number of steps taken towards the light as a percentage of MAX_RAY_STEPS -> then do 1 - that as pixel values are currently flipped (i.e.) 1 = black
-            float light_steps = (static_cast<float>(march_steps)/static_cast<float>(MAX_RAY_STEPS));
+            float light_intensity = max(0.0f, (normal[0]*light_dir[0] + normal[1]*light_dir[1] + normal[2]*light_dir[2]));
 
-            current_sample *= light_steps;
-
-            // Add diffuse lighting
-            current_sample += DIFFUSE_LIGHTING;
+            pixel[0] += LIGHT_COLOUR[0] * (light_intensity + DIFFUSE_LIGHTING);
+            pixel[1] += LIGHT_COLOUR[1] * (light_intensity + DIFFUSE_LIGHTING);
+            pixel[2] += LIGHT_COLOUR[2] * (light_intensity + DIFFUSE_LIGHTING);
         }
         else
         {
-            current_sample = 0.0f;
+            pixel[0] += 0.0f;
+            pixel[1] += 0.0f;
+            pixel[2] += 0.0f;
         }
-
-        pixel[0] += current_sample;
-        pixel[1] += current_sample;
-        pixel[2] += current_sample;
     }
 
     // // Average pixel
@@ -234,22 +273,22 @@ int main() {
     const float screen_top_left_temp[3] = {-tan(0.5f * H_FOV), -tan(0.5f * V_FOV), -1};
     const float screen_bottom_right_temp[3] = {tan(0.5f * H_FOV), tan(0.5f * V_FOV), -1};
     const float light_pos_temp[3] = {0, -10, 3};
+    const float light_col_temp[3] = {1.0f, 0.6353f, 0.2235f}; // 3100K light
     cudaMemcpyToSymbol(CAM_POS, cam_pos_temp, 3 * sizeof(float));
     cudaMemcpyToSymbol(SCREEN_TOP_LEFT, screen_top_left_temp, 3 * sizeof(float));
     cudaMemcpyToSymbol(SCREEN_BOTTOM_RIGHT, screen_bottom_right_temp, 3 * sizeof(float));
     cudaMemcpyToSymbol(LIGHT_POS, light_pos_temp, 3 * sizeof(float));
+    cudaMemcpyToSymbol(LIGHT_COLOUR, light_col_temp, 3 * sizeof(float));
                 
-    // Note, currently too many threads will result in region of image not being computed
     int cuda_threads = std::min(WIDTH*HEIGHT, MAX_CUDA_THREADS);
     int blockSize = 256;
     int gridSize = (cuda_threads + blockSize - 1) / blockSize;
-    // std::cout << grid_size << std::endl;
 
     curandState_t* rand_state;
     cudaMalloc(&rand_state, sizeof(curandState_t) * (gridSize*blockSize));
     init_kernel<<<gridSize,blockSize>>>(rand_state);
 
-    for (int frame_idx = 0; frame_idx < N_FRAMES; frame_idx++)
+    for (int frame_idx = FRAME_START; frame_idx < N_FRAMES; frame_idx++)
     {
         print_progress(frame_idx);
 
@@ -257,7 +296,10 @@ int main() {
         float time = static_cast<float>(frame_idx * MS_PER_FRAME) / 1000.0f;
 
         // Run frame compute kernel
-        compute_pixel<<<gridSize,blockSize>>>(cu_pixels, rand_state, time);
+        for (unsigned int pixel_offset = 0; pixel_offset < WIDTH*HEIGHT; pixel_offset += cuda_threads)
+        {
+            compute_pixel<<<gridSize,blockSize>>>(pixel_offset, cu_pixels, rand_state, time);
+        }
         cudaMemcpy(host_pixels, cu_pixels, 3*WIDTH*HEIGHT, cudaMemcpyDeviceToHost);
 
         cudaError_t err = cudaGetLastError();
@@ -265,17 +307,8 @@ int main() {
             printf("Error: %s\n", cudaGetErrorString(err));
 
         // Write PPM file
-        std::ofstream file;
-        file.open (std::string("frames/") + std::to_string(frame_idx) + std::string(".ppm"));
-        file << "P3 " << WIDTH << " " << HEIGHT <<  " 256\n";
-        for(int y = 0; y < HEIGHT; y++) {
-            for(int x = 0; x < WIDTH; x++) {
-                uint8_t* pixel = &host_pixels[3*(y*WIDTH + x)];
-                file << (int)pixel[0] << " " << (int)pixel[1] << " " << (int)pixel[2] << " ";
-            }
-            file << "\n";
-        }
-        file.close();
+        std::string filename = std::string("frames/") + std::to_string(frame_idx - FRAME_START) + std::string(".png");    
+        (void)stbi_write_png(filename.c_str(), WIDTH, HEIGHT, 3, host_pixels, 0);
     }
 
     // Cleanup
